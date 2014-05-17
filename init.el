d62cb9fb55745777a58448e56dd28e56218bd8bb
David Mann <mannd@epstudiossoftware.com>
2014-05-17 15:18:34 +0200

    took out added packages for now

--- init.el
+++ init.el
@@ -1,10 +1,21 @@
 ;;
+;; ~/.emacs.d/init.el
+;; David Mann
 ;; new init file for latest emacs, latest org-mode
-;; org mode
 ;;
+;;
+;; add package archives
+;; stick with gnu packages for now
+;; (add-to-list 'load-path "~/.emacs.d")
+;; (require 'use-package)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; org mode
 (add-to-list 'load-path (expand-file-name "~/git/org-mode/lisp"))
 (add-to-list 'load-path (expand-file-name "~/git/org-mode/contrib/lisp"))
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+
+;; (package-initialize t)
 (require 'org)
 ;;
 ;; Standard key bindings
@@ -14,6 +25,11 @@
 (setq org-agenda-include-diary t)
 (add-hook 'org-mode-hook 'turn-on-font-lock)
 (setq org-hide-leading-stars 'hidestars) ; just one star visible
+;;
+;; For mobile org
+(setq org-directory "~")
+(setq org-mobile-inbox-for-pull "~/todo.org")
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 ;;
 ;; Personalization
 (setq user-mail-address "mannd@epstudiossoftware.com")
@@ -25,6 +41,12 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 ;; eliminate splash screen
 (setq inhibit-splash-screen t)
+;; No backup files
+(setq make-backup-files nil)
+;; Show column number
+(column-number-mode t)
 ;;
 ;; Let's load a nice color-theme
 (load-theme 'tsdh-light t)
+;;
+

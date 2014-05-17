028e9155ea1a6e7003ccadd45060a938ce8f2123
David Mann <mannd@epstudiossoftware.com>
2014-05-17 14:43:52 +0200

    added .gitignore, added usable color-theme for aquamacs

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 ;; org mode
 ;;
 (add-to-list 'load-path (expand-file-name "~/git/org-mode/lisp"))
+(add-to-list 'load-path (expand-file-name "~/git/org-mode/contrib/lisp"))
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 (require 'org)
 ;;
@@ -10,3 +11,20 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+(setq org-agenda-include-diary t)
+(add-hook 'org-mode-hook 'turn-on-font-lock)
+(setq org-hide-leading-stars 'hidestars) ; just one star visible
+;;
+;; Personalization
+(setq user-mail-address "mannd@epstudiossoftware.com")
+;;
+;; Some necessities from old .emacs
+;; Lose menus, toolbars, scrollbars
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;; eliminate splash screen
+(setq inhibit-splash-screen t)
+;;
+;; Let's load a nice color-theme
+(load-theme 'tsdh-light t)

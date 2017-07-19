7a2151d816b4fddb9b65fcc7b97571318dc1d9b0
David Mann <mannd@epstudiossoftware.com>
2017-07-19 09:19:17 -0600

    Remove deprecated org-iswitchb function

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cb" 'org-switchb)
 (setq org-agenda-include-diary t)
 ;; ~/org is a symlink to "~/Dropbox/org" for easy sharing
 (setq org-directory "~/org")

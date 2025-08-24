c2041146fbcdb8e5f04ec44b60eedff954b0d80d
David Mann <mannd@epstudiossoftware.com>
2025-08-23 21:37:25 -0600

    Last commit before losing history and switching to .dotfiles repo

--- init.el
+++ init.el
@@ -16,6 +16,23 @@
 
 ;; Load my literate Emacs configuration.
 (org-babel-load-file "~/.emacs.d/configuration.org")
+;; (let* ((org-file "~/.emacs.d/configuration.org")
+;;        (el-file  "~/.emacs.d/configuration.el")
+;;        (elc-file "~/.emacs.d/configuration.elc"))
+;;   ;; Tangle if .org is newer than .el
+;;   (when (file-newer-than-file-p org-file el-file)
+;;     (require 'org)
+;;     (org-babel-tangle-file org-file el-file))
+
+;;   ;; Byte-compile if .el is newer than .elc
+;;   (when (file-newer-than-file-p el-file elc-file)
+;;     (byte-compile-file el-file))
+
+;;   ;; Load the compiled version if it exists, otherwise the plain .el
+;;   (if (file-exists-p elc-file)
+;;       (load elc-file)
+;;     (load el-file)))
+
 
 ;; Finish timing the startup.
 ;; THIS NEEDS TO BE LAST IN init.el.
